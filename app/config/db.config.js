86b6a89526ac7400e6b3a6bef6b937c2bffdc8e7
Harsha Hirushan <janith.h@softcodeit.com>
2023-12-10 08:51:58 +0530

    added intial

---
+++ app/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // url: "mongodb+srv://himesh:himesh@banking.1gft4kp.mongodb.net/?retryWrites=true&w=majority"
+    url: "mongodb+srv://harsha:harsha@cluster0.tqz232f.mongodb.net/?retryWrites=true&w=majority"
+};
